5b4aaac8794e57daf7feecc492740911919eeaa6
Sampath <kumar@Sampaths-Laptop.lan>
2024-09-30 01:20:52 -0500

    cahnges made

--- 05-array.sh
+++ 05-array.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-FRUITS=("APPLE" "BANANA" "ORANGE")
+FRUITS=("APPLE" "BANANA" "ORANGE" "MANGO")
 
 echo "First fruit is : ${FRUITS[0]}"
 echo  "second fruit is : ${FRUITS[2]}"
 echo "third fruit is : ${FRUITS[3]}"
 
-echo "First fruit is : ${FRUITS[@]}
+echo "First fruit is : ${FRUITS[@]}"
