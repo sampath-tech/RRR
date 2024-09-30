9a6572788263e798127cc85a0547a92f86753e0e
Sampath <kumar@Sampaths-Laptop.lan>
2024-09-30 00:41:24 -0500

    cahnges made

---
+++ 03-varaibles.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1 : :hi $PERSON2 How are you doing"
+echo "$PERSON2 : : Im doing $PERSON1 How are you"
+echo "$PERSON1 : : What are you doing $PERSON2"
+echo "$PERSON2: : yet to start marketing $PERSON2"
