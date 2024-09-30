4680daab703f672e3be0bb2d58684b671d6d1262
Sampath <kumar@Sampaths-Laptop.lan>
2024-09-30 01:17:09 -0500

    cahnges made

---
+++ 05-array.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+FRUITS=("APPLE" "BANANA" "ORANGE")
+
+echo "First fruit is : ${FRUIT[0]}"
+echo  "second fruit is : ${FRUIT[2]}"
+echo "third fruit is : ${FRUIT[3]}"
+
+echo "First fruit is : ${FRUIT[@]}
