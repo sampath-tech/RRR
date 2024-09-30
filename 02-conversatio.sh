5bea2985ccdd792dd7db8fe3a794070b678bb4c5
Sampath <kumar@Sampaths-Laptop.lan>
2024-09-30 00:19:15 -0500

    new conversation script added

---
+++ 02-conversatio.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Sampath : :hi Pavan How are you doing"
+echo "Pavan : : Im doing sampath How are you"
+echo "sampath : : What are you doing pavan"
+echo "Pavan: : yet to start marketing"
