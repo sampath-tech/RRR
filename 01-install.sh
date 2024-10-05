a35abb956f22836c06e490509a1442caf9c47422
Sampath <kumar@Sampaths-Laptop.lan>
2024-10-05 01:27:22 -0500

    reddy

--- 01-install.sh
+++ 01-install.sh
@@ -6,9 +6,9 @@ USERID=$(id -u)
 
 echo "userid is $USERID"
 
-#if [ $USERID -ne 0 ]
-#then
-    #echo "please run with the root access"
-#fi
+if [ $USERID -ne 0 ]
+then
+    echo "please run with the root access"
+fi
 
 dnf install mysqld -y
