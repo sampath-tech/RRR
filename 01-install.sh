a3f08d342e6aac4fcc29ce39c9322a03fc133f7e
Sampath <kumar@Sampaths-Laptop.lan>
2024-10-05 01:15:47 -0500

    reddy

---
+++ 01-install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#This script is only to check the user id only
+
+USERIS=$(id -u)
+
+echo "userid is $USERID"
+
+if [ $USERID -ne 0 ]
+
+then
+    echo "please run with the root access"
+
+fi
+
+dnf install mysqld -y
