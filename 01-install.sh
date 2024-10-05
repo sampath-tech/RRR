44428c73f8a0941504a4cb72eaff34ba99aad050
Sampath <kumar@Sampaths-Laptop.lan>
2024-10-05 01:41:21 -0500

    reddy

--- 01-install.sh
+++ 01-install.sh
@@ -6,10 +6,10 @@ USERID=$(id -u)
 
 echo "userid is $USERID"
 
-if [ $USERID -eq 0 ]
+if [ $USERID -ne 0 ]
 then
     echo "please run with the root access"
-    exit 0
+    exit 1
 fi
 
 dnf list installed git -y
